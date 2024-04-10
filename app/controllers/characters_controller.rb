b4568aa9983eb85ad542d92e2a4a8270432efe67
TALE-NYK <nayuka.tale@gmail.com>
2024-04-10 23:19:22 +0900

    キャラクタページの表示などを修正

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -8,7 +8,7 @@ class CharactersController < ApplicationController
 
   # GET /characters/1 or /characters/1.json
   def show
-    @tune_list = character_tunes.where("character_id = #{character.id}")
+    @tune_list = CharacterTune.where("character_id = #{@character.id}")
   end
 
   # GET /characters/new
